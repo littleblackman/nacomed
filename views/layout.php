8b4b2262dce9bcaef14bba8db945f7865e84f8a2
MaxL34 <mlauvergeat@gmail.com>
2019-05-23 10:18:31 +0200

    Mise en place du layout

---
+++ views/layout.php
@@ -0,0 +1,75 @@
+<?php
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Association Nacomed</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+        <!-- Google fonts-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo:400,400i">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+        <!-- Lightbox-->
+        <link rel="stylesheet" href="vendor/lightbox2/css/lightbox.min.css">
+        <!-- Font Awesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Parallax-->
+        <link rel="stylesheet" href="vendor/onepage-scroll/onepage-scroll.css">
+        <!-- theme stylesheet-->
+        <link rel="stylesheet" href="css/style.blue.css" id="theme-stylesheet">
+        <!-- Custom stylesheet - for your changes-->
+        <link rel="stylesheet" href="css/custom.css">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="favicon.png">
+        <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    </head>
+    <body>
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+          <div class="container">
+          <a class="navbar-brand" href="index.html">NACOMED</a>
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i class="fas fa-bars"></i>
+          </button>
+          
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=displayAbout">Actualités</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=listArticles">Embarquement</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=displayContact">Nous soutenir</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./index.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./index.php">Communauté</a>
+              </li>
+              </ul>
+          </div>
+          </div>
+      </nav>
+
+      <?= $content ?>
+
+      <footer>
+        
+
