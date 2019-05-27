e1bfcfb3b6c19c37477bfe9fb8656c328034668f
MaxL34 <mlauvergeat@gmail.com>
2019-05-27 18:34:12 +0200

    Ajout du code permettant d'afficher la page home

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
-require('./controllers/frontController.php');
-require('./controllers/adminController.php');
+    require('../controllers/frontController.php');
+    require('../controllers/adminController.php');
+
+    if (!empty($_GET['action'])) {
+        switch ($_GET['action']) {
+            case 'displayHome':
+                displayHome();
+            break;
+
+            default:
+                displayHome();
+        }
+    } else {
+        displayHome();
+    }
+
 
