2f44d58bbcc2cfb008454d63b90d5deb07ac469e
MaxL34 <mlauvergeat@gmail.com>
2019-05-30 08:52:28 +0200

    MOdifiation de la section 2, ajout de paragraphes, icones

--- public/views/frontend/home.php
+++ public/views/frontend/home.php
@@ -7,18 +7,21 @@
         <div class="dark-overlay"></div>
             <div class="position-relative z-index-1">
                 <div class="container welcome text-center text-white">
-                    <p class="font-italic lead">Bienvenue sur le site de Nacomed !</p>
+                    <p class="font-italic lead">Bienvenue sur le site de : </p>
                     <h1 class="text-uppercase my-4">NACOMED</h1>
                     <h2>Navire d'&Eacute;tudes Corse Méditerranée</h2>
-                    <p class="font-italic lead">Une association loi 1901 à but non lucratif</p>
-                    <p class="font-italic lead">Un concept de navigation pour prendre plus de plaisir en navigant utile, en provoquant les rencontres humaines pour partager et s'enrichir humainement.</p>
+                    <p class="font-italic lead">Un savant mélange d’années passées dans la navigation de plaisance, la marine marchande, en école et en asso…</p>
+                    <p class="font-italic lead">Un navire école pour professionnels et amateurs, sur un concept de navigation pour prendre plus de plaisir en naviguant utile, 
+                                                à la voile, en provoquant les rencontres et les échanges pour s’enrichir humainement en œuvrant pour préserver notre patrimoine 
+                                                marin et maritime.
+                    </p>
                 </div>
 
                 <div class="nav_infos text-white col-lg-4">
                     <span class="asso_info"><i class="fas fa-angle-double-right"></i>Navigation éco-responsable</span>
-                    <span class="asso_info"><i class="fas fa-angle-double-right"></i>Apprendre en faisant</span>
+                    <span class="asso_info"><i class="fas fa-angle-double-right"></i>Apprendre en navigant</span>
                     <span class="asso_info"><i class="fas fa-angle-double-right"></i>Accomplir des missions scientifiques</span>
-                    <span class="asso_info"><i class="fas fa-angle-double-right"></i>Transmettre son savoir</span>
+                    <span class="asso_info"><i class="fas fa-angle-double-right"></i>Partager son savoir</span>
                 </div>
 
             </div>
@@ -27,7 +30,7 @@
     </section>
     <section>
         <div class="d-flex h-100">
-            <div class="container">
+            <div class="container about_div">
                 <div class="row">
                     <div class="col-lg-5 science-boat">
                         <header class="text-center">
@@ -35,28 +38,20 @@
                                 <p class="lined">&Eacute;tudier le milieu marin</p>
                         </header>
                             <div class="boat-info"><img src="https://img.icons8.com/ios/50/000000/microscope.png"><p class="about-title short-p">Accueillir des missions scientifiques</p></div>
-                            <div class="boat-info"><img src="https://img.icons8.com/ios/50/000000/graph.png"><p class="about-title">Effectuer des relevés en autonomie (plancton, micorplastique, accoustique</p></div>
-                            <div class="boat-info"><img src="../img/plancton.png"><p class="about-title short-p">S'intégrer à des programmes de recherches</p></div>
-                            <div class="boat-info"><img src="https://img.icons8.com/dotty/50/000000/flounder-fish.png"><p class="about-title">Participer aux réseaux de sciences participatives en mer (OBSenMER, ...)</p></div>
+                            <div class="boat-info"><img class="sized_img" src="https://img.icons8.com/ios/50/000000/graph.png"><p class="about-title">Effectuer des relevés en autonomie (plancton, micorplastique, accoustique</p></div>
+                            <div class="boat-info"><img src="./img/plancton.png"><p class="about-title short-p">S'intégrer à des programmes de recherches</p></div>
+                            <div class="boat-info"><img class="sized_img" src="https://img.icons8.com/dotty/50/000000/flounder-fish.png"><p class="about-title short-p">Participer aux réseaux de sciences participatives en mer (OBSenMER, ...)</p></div>
                     </div>
-                    <div class="col-lg-2"><img src="../img/logo_nacomed.png" alt="..." class="img-fluid rounded-circle d-block mx-auto about-logo"></div>
+                    <div class="col-lg-2"><img src="./img/logo_nacomed.png" alt="..." class="img-fluid rounded-circle d-block mx-auto about-logo"></div>
                     <div class="col-lg-5 ecology-boat">
                         <header class="text-center">
                             <h3 class="text-uppercase">Un navire écologique</h2>
                             <p class="lined">Préserver et sensibiliser</p>
                         </header>
-                            <div class="boat-info ecology"><img src="https://img.icons8.com/dotty/50/000000/waste.png"><p class="about-title">Effectuer des dépollutions et des nettoyages</p></div>
-                            <div class="boat-info special-div ecology"><img src="https://img.icons8.com/dotty/50/000000/commercial.png">
-                                <!-- <div class="sensibilisation-div"> -->
-                                    <p class="about-title short-p">Sensibilisation :</p></div>
-                                        <p class="about-subtitle"><i class="fas fa-angle-right"></i>Dans les ports</p>
-                                        <p class="about-subtitle"><i class="fas fa-angle-right"></i>Auprès des plaisanciers</p>
-                                        <p class="about-subtitle"><i class="fas fa-angle-right"></i>Auprès des plagistes</p>
-                                        <p class="about-subtitle"><i class="fas fa-angle-right"></i>Auprès des écoliers et étudiants</p>
-                                <!-- </div> -->
-                            <div class="boat-info special-div ecology"><img src="https://img.icons8.com/dotty/50/000000/light-on.png">
-                                <p class="about-title short-p">Technologie :</p></div>
-                                <p class="about-subtitle"><i class="fas fa-angle-right"></i>Développer l'autonomie du navire grâce au Low-tech</p>
+                            <div class="boat-info"><img src="https://img.icons8.com/dotty/50/000000/compass.png"><p class="about-title short-p">Naviguer et s’avitailler de manière éco-responsable</p></div>
+                            <div class="boat-info"><img src="https://img.icons8.com/ios/50/000000/atom-editor.png"><p class="about-title">Développer l’autonomie du navire grâce aux low-techs</p></div>
+                            <div class="boat-info"><img class="sized_img" src="https://img.icons8.com/dotty/50/000000/commercial.png"><p class="about-title"> Mener des actions de sensibilisation auprès des plaisanciers, des plagistes, des écoliers et étudiants, et du grand public</p></div>
+                            <div class="boat-info"><img src="https://img.icons8.com/ios/50/000000/recycle-sign.png"><p class="about-title short-p">Effectuer des dépollutions et nettoyages</p></div>
                     </div>
                 </div>
 
@@ -71,15 +66,22 @@
                                    mer plus sûre, pour savoir l'apprécier et pour la respecter.
                                 </p>
                             <h5>Tous les niveaux sont acceptés !</h5>
-                                <p>Les futurs marins professionnels (capitaines et lieutenants 200 UMS) pourront apprendre leur métier en mer en participant à toutes les tâches 
-                                    du bord et en se responsabilisant. Avec un rôle d’équipage ils auront la possibilité de valider leur temps de mer à bord auprès de la DIRM 
-                                    afin de les soutenir dans leur cursus d’étudiant. Encore une fois l’objectif est de former des personnes qualifiées et écoresponsable pour 
-                                    rendre la mer plus sûre et sans l’abimer ; cela grâce à des navigations encadrées sur un navire spécialisé et des rencontres enrichissantes. 
-                                    Ils sont les futurs acteurs du milieu maritime !
+                                <p><strong>Les éco-volontaires</strong> (du débutant curieux à l’amateur confirmé) se joindront au bord pour découvrir, apprendre, se perfectionner 
+                                   et s’affirmer.<br>
+                                   <strong>Les futurs marins</strong> professionnels (capitaines et lieutenants 200 UMS) pourront apprendre leur métier en mer en participant à toutes les tâches
+                                   du bord et en se responsabilisant. Avec un rôle d’équipage ils auront la possibilité de valider leur temps de mer à bord auprès de la DIRM
+                                   afin de les accompagner dans leur cursus d’étudiant. L’objectif est de former des personnes qualifiées et écoresponsables pour rendre la mer
+                                   plus sûre et la préserver ; cela grâce à des navigations encadrées sur un navire spécialisé. Ils sont les futurs acteurs du milieu maritime !
                                 </p>
-
+                            <h5>Partager nos connaissances et nos expériences, une priorité !</h5>
+                                <p>La bonne ambiance à bord est essentielle, et nous entretiendrons un échange permanent et convivial entre tous les membres du navire, afin que
+                                   chacun puisse apporter ses compétences et ses savoirs pour l’enrichissement personnel et le bon déroulement de chacune  des missions.<br>
+                                   Nous interagissons avec d’autres associations corses portées sur la protection du milieu marin, et nous continuerons à développer ces relations 
+                                   jusqu’aux autres associations méditerranéennes.<br>
+                                   Enfin, notre voilier et son équipage seront toujours ouverts à l’échange et l'entraide sur tous ces sujets que sont aujourd’hui les enjeux
+                                   environnementaux et maritimes de méditerranée. Alors contactez-nous !
+                                </p>
                         </div>
-
             </div>
         </div>
     </section>
@@ -92,15 +94,34 @@
                 
                 <div class="carousel">
                     <div class="owl-carousel owl-theme">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_jour.jpg" alt="" style="height: 600px; width: 100%">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_profil_gauche.jpg" alt="" style="height: 600px; width: 100%">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_nuit.jpg" alt="" style="height: 600px; width: 100%">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_crepuscule.jpg" alt="" style="height: 600px; width: 100%">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_pont.jpg" alt="" style="height: 600px; width: 100%">
-                        <img class="owl-lazy" data-src="../img/sonate/sonate_pont_2.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_jour.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_profil_gauche.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_nuit.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_crepuscule.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_pont.jpg" alt="" style="height: 600px; width: 100%">
+                        <img class="owl-lazy" data-src="./img/sonate/sonate_pont_2.jpg" alt="" style="height: 600px; width: 100%">
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
+    <section class="bg-gray">
+        <div class="d-flex h-100">
+          <div class="container">
+            <header class="text-center mb-5">
+              <h2 class="text-uppercase lined">Le navire en détails</h2>
+            </header>
+            <div class="row">
+              <div class="col-lg-12">
+                <p>Able an hope of body. Any nay shyness article matters own removal nothing his forming. Gay own additions education satisfied the perpetual. If he cause manor happy. Without farther she exposed saw man led. Along on happy could cease green oh.</p>
+                <p>Betrayed cheerful declared end and. Questions we additions is extremely incommode. Next half add call them eat face. Age lived smile six defer bed their few. Had admitting concluded too behaviour him she. Of death to or to being other.</p>
+              </div>
+              <div class="col-lg-12">
+                <p>Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed.</p>
+                <p>Saw yet kindness too replying whatever marianne. Old sentiments resolution admiration unaffected its mrs literature. Behaviour new set existence dashwoods. It satisfied to mr commanded consisted disposing engrossed. Tall snug do of till on easy. Form not calm new fail.</p>
+              </div>
+            </div>
+          </div>
         </div>
     </section>
     <section>
@@ -110,7 +131,7 @@
                     <h2 class="text-uppercase lined">Contact</h2>
                 </header>
                 <div class="row">
-                    <div class="col-lg-12"><img src="../img/logo_nacomed.png" alt="..." class="img-fluid rounded-circle d-block mx-auto contact-logo"></div>
+                    <div class="col-lg-12"><img src="./img/logo_nacomed.png" alt="..." class="img-fluid rounded-circle d-block mx-auto contact-logo"></div>
                 </div>
                 <div class="row lined">
                     <div class="col-lg-6 person_infos text-center">
@@ -120,7 +141,7 @@
                         <p>Mail : <a href="mailto:navirecorsemed@gmail.com">navirecorsemed@gmail.com</a></p>
                     </div>
                     <div class="col-lg-6 text-center person_pic">
-                        <img src="../img/cyrille_resized.png">
+                        <img src="./img/cyrille_resized.png">
                     </div>
                 </div>
 
@@ -131,10 +152,10 @@
                         <p>Mail : <a href="mailto:mlauvergeat@gmail.com">mlauvergeat@gmail.com</a></p>
                     </div>
                     <div class="col-lg-6 text-center person_pic">
-                        <img src="../img/max_resized.png">
+                        <img src="./img/max_resized.png">
                     </div>
                 </div>
-            <?php require('views/footer.php'); ?>
+            <?php require('./public/views/footer.php'); ?>
             </div>
         </div>
     </section>
@@ -143,4 +164,4 @@
 <!-- <script src="../vendor/owl.carousel/owl.carousel.js"></script> -->
 
 <?php $content = ob_get_clean(); ?>
-<?php require('views/layout.php'); ?>
+<?php require('./public/views/layout.php'); ?>
