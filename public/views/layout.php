df96d5c5135dc440a7675068d14f109f5bc098cc
MaxL34 <mlauvergeat@gmail.com>
2019-05-31 16:50:04 +0200

    Ajout de classes pour règles de styles

--- public/views/layout.php
+++ public/views/layout.php
@@ -41,8 +41,8 @@
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="./index.php"><img src="./img/logo_nacomed.png" /></a>
-                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="logo navbar-brand" href="./index.php"><img src="./img/logo_nacomed.png" /></a>
+                    <button class="menu_btn navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         Menu
                         <i class="fas fa-bars"></i>
                     </button>
