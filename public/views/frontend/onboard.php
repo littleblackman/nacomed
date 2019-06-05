a0544a8cfcc9c5aed54c53a93bc8ad2cb9a94eee
MaxL34 <mlauvergeat@gmail.com>
2019-06-05 10:34:13 +0200

    Inclusion du header

--- public/views/frontend/onboard.php
+++ public/views/frontend/onboard.php
@@ -27,38 +27,7 @@
     </head>
     <body>
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top onboard-nav" id="mainNav">
-            <div class="container">
-                <a class="logo navbar-brand" href="./index.php"><img src="./img/logo_nacomed.png" /></a>
-                    <button class="menu_btn navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                        Menu
-                        <i class="fas fa-bars"></i>
-                    </button>
-          
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="./index.php">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=displayAbout">Actualités</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=displayOnboard">Embarquement</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=displayContact">Nous soutenir</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="./index.php">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="./index.php">Communauté</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
+        <?php require('./public/views/header.php'); ?>
 
         <div class="eco-volunteer">
             <header class="text-center">
