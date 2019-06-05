fb59f0e240d709a0cf5c88e51dbed734a583c150
MaxL34 <mlauvergeat@gmail.com>
2019-06-05 10:32:03 +0200

    Création du fichier qui conteitn le menu de navigation

---
+++ public/views/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top onboard-nav" id="mainNav">
+    <div class="container">
+        <a class="logo navbar-brand" href="./index.php"><img src="./img/logo_nacomed.png" /></a>
+            <button class="menu_btn navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+    
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=displayContact">Actualités</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=displayOnboard">Embarquement</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?action=displayContact">Nous soutenir</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?action=displayContact">Contact</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?action=displayContact">Communauté</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
