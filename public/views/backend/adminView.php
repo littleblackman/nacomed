e1d8d220f5502597455fd125f0676d0fd4d2b18f
MaxL34 <mlauvergeat@gmail.com>
2019-06-11 12:20:58 +0200

    AJout des scripts JS

--- public/views/backend/adminView.php
+++ public/views/backend/adminView.php
@@ -26,3 +26,48 @@
         <!-- Navigation -->
         <?php require('./public/views/header.php'); ?>
 
+        <div class="container admin_interface">
+            <div class="row mgmt">
+                <div class="col-lg-12 text-center">
+                    <header>
+                        <h1>Espace d'administration</h1>
+                    </header>
+
+                    <p class="font-italic lead">
+                        Bienvenue sur votre espace d'administration, sélectionnez une option
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 text-center">
+                    <header>
+                        <h4>Gestion des news (articles)</h4>
+                            <button id="news-mgmt">Gérer</button>
+                    </header>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <header>
+                        <h4>Gestion de la map</h4>
+                            <button id="map-mgmt">Gérer</button>
+                    </header>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <header>
+                        <h4>Gestion des commentaires</h4>
+                            <button id="com-mgmt">Gérer</button>
+                    </header>
+                </div>
+        </div>
+
+
+        <?php require('./public/views/footer.php'); ?>
+
+    <!-- JavaScript files-->
+    <script src="./vendor/jquery/jquery/jquery.min.js"></script>
+    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./js/scroll_nav.js"></script>
+    <script src="./js/adminOptions.js"></script>
+    <script src="./js/admin.js"></script>
