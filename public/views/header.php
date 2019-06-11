5c7c58f99c1ed790a1446fb977aa1b4acdcc9cf0
MaxL34 <mlauvergeat@gmail.com>
2019-06-11 17:07:09 +0200

    Modification des liens du menu

--- public/views/header.php
+++ public/views/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top onboard-nav" id="mainNav">
     <div class="container">
-        <a class="logo navbar-brand" href="./index.php"><img src="./img/logo_nacomed.png" /></a>
+        <a class="logo navbar-brand" href="/nacomed/index.php"><img src="/nacomed/img/logo_nacomed.png" /></a>
             <button class="menu_btn navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fas fa-bars"></i>
@@ -9,25 +9,25 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php">Accueil</a>
+                    <a class="nav-link" href="/nacomed/index.php">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php?action=displayNews">Actualités</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=displayNews">Actualités</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php?action=displayOnboard">Embarquement</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=displayOnboard">Embarquement</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php?action=displayContact">Nous soutenir</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=displayContact">Nous soutenir</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php?action=displayContact">Contact</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=displayContact">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./index.php?action=displayContact">Communauté</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=displayContact">Communauté</a>
                 </li>
                 <li class="nav-item login">
-                    <a class="nav-link" href="./index.php?action=displayAdmin">Connexion</a>
+                    <a class="nav-link" href="/nacomed/index.php?action=login">Connexion</a>
                 </li>
             </ul>
         </div>
