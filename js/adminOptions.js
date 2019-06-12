7c017af458d1e4a60e4d1967ba5734c0ca37fca0
MaxL34 <mlauvergeat@gmail.com>
2019-06-12 11:57:51 +0200

    Ajout d'une nouvelle variable

--- js/adminOptions.js
+++ js/adminOptions.js
@@ -2,12 +2,17 @@ var option = {
     newsMgmtBtn: $('#news-mgmt'),
     mapMgmtBtn: $('#map-mgmt'),
     comMgmtBtn: $('#com-mgmt'),
+    newsCreationBtn: $('#news-creation'),
 
     init: function() {
         var self = this;
 
         self.newsMgmtBtn.click(function() {
             window.location.href = "/nacomed/public/views/backend/newsManagementView.php";
+        });
+
+        self.newsCreationBtn.click(function() {
+            window.location.href = "/nacomed/public/views/backend/newsCreationView.php";
         });
     }
 
