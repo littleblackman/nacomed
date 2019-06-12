d28c2c559c635d6d6cfc4bb6bee604e61d46a60f
MaxL34 <mlauvergeat@gmail.com>
2019-06-12 09:34:52 +0200

    Création des fichiers

---
+++ js/commentAdder.js
@@ -0,0 +1,70 @@
+var commentAdder = {
+    addComButton: $('#add_com'),
+    comForm: $('#com_form'),
+    comAuthor: $('#author'),
+    missPseudo: $('#missPseudo'),
+    comContent: $('#content'),
+    artId: $('#art_id').attr('value'),
+    missCom: $('#missCom'),
+    comSubmitBtn: $('#com_submit_btn'),
+    modalCom: $('#modal_com'),
+    modalText: $('#modal_text'),
+
+    init: function() {
+        var self = this;
+
+        self.comForm.hide();
+
+        self.addComButton.click(function() {
+            self.comForm.show();
+        });
+
+        self.comSubmitBtn.click(function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            /*$(document).click(function(event) { 
+                if(!$(event.target).closest(self.modalCom).length) {
+                    self.modalCom.hide();
+                    self.modalCom.stop(true, true).fadeOut();
+                } 
+            });*/
+
+            $.ajax({
+                url: '/nacomed/index.php?action=addComment',
+                type: 'POST',
+                data: 'com_content=' + self.comContent.val() + '&com_author=' + self.comAuthor.val() + '&art_id=' + self.artId,
+                dataType: 'text',
+                success: function(data) {
+                    console.log('data = ' + data);
+                    if (data == 'success') {
+                        self.modalText.text('Votre commentaire à bien été ajouté');
+                        self.modalCom.show();
+                        self.modalCom.fadeOut(4000, function() {
+                            self.modalCom.hide();
+                            window.location.href = "/nacomed/index.php?action=getArticle&article_id=" + self.artId;
+                        });
+                    } else if (data == 'author_missing') {
+                        self.modalText.text('Pseudo manquant : veuillez remplir ce champ');
+                        self.modalCom.show();
+                        self.modalCom.fadeOut(4000, function() {
+                            self.modalCom.hide();
+                        });
+                    } else if (data == 'content_missing') {
+                        self.modalText.text('Commentaire vide : écrivez un commentaire');
+                        self.modalCom.show();
+                        self.modalCom.fadeOut(4000, function() {
+                            self.modalCom.hide();
+                        });
+                    } else if (data == 'id_error') {
+                        self.modalText.text('Erreur de récupération de l\'id de billet : contactez votre webmaster');
+                        self.modalCom.show();
+                        self.modalCom.fadeOut(4000, function() {
+                            self.modalCom.hide();
+                        });
+                    }
+                }
+            });
+        });
+    }
+};
---
+++ js/reportCom.js
@@ -0,0 +1,53 @@
+var comToReport = {
+    comReportBtn: $('.report_com_btn'),
+    modalCom: $('#modal_com'),
+    modalText: $('#modal_text'),
+    comText: $('.com_text'),
+    artId: $('#art_id').attr('value'),
+
+    init: function() {
+        var self = this;
+
+        if ($('#comments span').hasClass('span_censored')) {
+            $('.span_censored').text('Commentaire signalé');
+            } else {
+                $('.span_censored').text('');    
+            }
+
+        self.comReportBtn.click(function(e) {
+            var comId = $(this).attr('id');
+            var artId = $('#art_id').attr('value');
+
+            self.resetModal();
+
+            $.ajax({
+                url: '/nacomed/index.php?action=reportCom',
+                type: 'GET',
+                data: 'article_id=' + artId + '&com_id=' + comId,
+                dataType: 'text',
+                success: function() {
+                    self.modalText.text('Le commentaire a bien été signalé à l\'administrateur');
+                    self.modalCom.show();
+                    self.modalCom.fadeOut(4000, function() {
+                        self.modalText.text('');
+                        self.modalCom.hide();
+                        window.location.href = "/nacomed/index.php?action=getArticle&article_id=" + self.artId;
+                    });
+                }
+            });
+            e.preventDefault();
+        });
+    },
+
+    resetModal: function() {
+        var self = this;
+
+        $(document).click(function(event) { 
+            if(!$(event.target).closest(self.modalCom).length) {
+                console.log('document cliqué');
+                self.modalCom.hide();
+                self.modalCom.stop(true, true).fadeOut();
+            } 
+        });
+    }
+};
