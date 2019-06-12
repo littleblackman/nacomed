9c37d9cc87fdc5eaebd79ef71e5d5e132c9b4f9d
MaxL34 <mlauvergeat@gmail.com>
2019-06-12 09:57:25 +0200

    Changement du chemin

--- public/views/frontend/newsView.php
+++ public/views/frontend/newsView.php
@@ -35,7 +35,75 @@
                     </div>
                 </div>
             </div>
-      </div>
+
+            <!-- Post Content -->
+            <article>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-10 mx-auto">
+                            <p><?php echo $article->art_content(); ?></p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <div id="add_com">
+                <button>Ajouter un commentaire</button>
+            </div>
+
+            <form id="com_form" action="/nacomed/index.php?action=addComment" method="post">
+                <div>
+                    <label for="com_author">Pseudo : </label>
+                    <input class="com_author" type="text" id="author" name="com_author" placeholder="Votre pseudo" required/>
+                    <span id="missPseudo"></span>
+                </div>
+
+                <div>
+                    <label for="com_content">Votre commentaire : </label>
+                    <textarea class="com_content" id="content" name="com_content" placeholder="Votre commentaire" required></textarea>
+                    <span id="missCom"></span>
+                </div>
+
+                <input type="hidden" id="art_id" name="art_id" value="<?php echo $article->art_id(); ?>" />
+
+                <div>
+                    <input id="com_submit_btn" type="submit" />
+                </div>
+            </form>
+
+            <div id="comments">
+                <p class="com_heading">Commentaires du billet :</p>
+                    <?php foreach ($comments as $comment) {
+                        $censoredCommentClass = $comment->com_report_number() > 0 ? "comment_censored" : "";
+                        $censoredSpanClass = $comment->com_report_number() > 0 ? "span_censored" : "";
+                    ?>
+                        <div class="comment_content <?= $censoredCommentClass ?>">
+                            <p class="com_text"><?php echo $comment->com_content(); ?></br></br>
+                                Posté par : <?php echo $comment->com_author(); ?></br>
+                                le : <?php echo $comment->com_date_fr(); ?></br>
+                                <input type="button" class="report_com_btn" value="Signaler" id="<?php echo $comment->com_id(); ?>"/>
+                            </p>
+                            <span class="<?= $censoredSpanClass ?>"></span>
+                        </div></br>
+                    <?php } ?>
+            </div>
+
+            <div id="modal_com" class="modal">
+                <div class="modal_content">
+                    <p id="modal_text"></p>
+                </div>
+            </div>
+        </div>
+
+        <?php require('./public/views/footer.php'); ?>
+
+        <!-- JavaScript files-->
+        <script src="./vendor/jquery/jquery/jquery.min.js"></script>
+        <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="./js/scroll_nav.js"></script>
+        <script src="./js/commentAdder.js"></script>
+        <script src="/nacomed/js/reportCom.js"></script>
+        <script src="./js/controllers/newsView.js"></script>
 
     </body>
 </html>
