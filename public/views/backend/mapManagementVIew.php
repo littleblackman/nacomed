22bae7b43d2cb6ae6bb87cbc20f09bffdc35243f
MaxL34 <mlauvergeat@gmail.com>
2019-06-14 17:33:18 +0200

    Ecriture du code pour la vue de la gestion de la map

---
+++ public/views/backend/mapManagementVIew.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Association Nacomed : Actualités</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+        <!-- Google fonts-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo:400,400i">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+        <!-- Font Awesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Custom stylesheet - for your changes-->
+        <link rel="stylesheet" href="./css/custom.css">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="favicon.png">
+        <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <?php require('./public/views/header.php'); ?>
+
+        <div class="container welcome-map">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <header>
+                        <h1>Bienvenue sur la page de gestion de la map</h1>
+                    </header>    
+                    <p>Placez un nouveau marqueur ou consulter la liste existante pour les éditer</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 actions">
+                    <button id="event-list">Liste des events</button>
+                </div>
+
+                <div class="col-lg-10 area-map">
+                    <form class="form-group" id="map-form" method="post" action="/nacomed/index.php?action=addMarker">
+                        <label for="name">Nom de l'évènement : </label><input class="form-control" type="text" id="name" name="name" /><br/>
+                        <label for="lng">Longitude : </label><input class="form-control" type="text" id="lng" name="lng" /><br/>
+                        <label for="lat">Lattitude : </label><input class="form-control" type="text" id="lat" name="lat" /><br>
+                        <label for="ville">Ville : </label><input class="form-control" type="text" id="ville" name="ville" /><br>
+
+                        <input type="submit" id="mapFormSubmit" name="mapFormSubmit" value="Envoyer" />
+                    </form>
+
+                    <div id="gmap" style="width:100%; height:600px">
+                    </div>
+                </div>      
+            </div>
+        </div>
+
+
+    <?php require('./public/views/footer.php'); ?>
+
+    <!-- JavaScript files-->
+    <script src="/nacomed/js/gmap.js"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMYjmQOsX1wZFkHJECVFrCZet0FJYK9kg&callback=initMap"></script>
+    
+
+    
+    
+    </body>
+</html>
