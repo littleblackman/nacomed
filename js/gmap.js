760105eec7001dc1ca7163e89baa02f005c1e812
MaxL34 <mlauvergeat@gmail.com>
2019-06-16 08:41:32 +0200

    Ajout du code pour le fonctionnement de la map

--- js/gmap.js
+++ js/gmap.js
@@ -1,15 +1,54 @@
 $(function() {
+    /* GMAP */
     var corte= {lat: 42.309409, lng: 9.149022};
-
+    
+    var map = new google.maps.Map(document.getElementById('gmap'), {
+        zoom: 8,
+        center: corte,
+        mapTypeId: google.maps.MapTypeId.ROADMAP 
+    });
 
     function initMap() {
-        var map = new google.maps.Map(document.getElementById('gmap'), {
-            zoom: 8,
-            center: corte,
-            mapTypeId: google.maps.MapTypeId.ROADMAP 
-        });
+        map;
     }
+        
 
     initMap();
 
+    var marker = new google.maps.Marker({
+        position: corte,
+        map: map,
+        title: 'Bougez le marqueur',
+        draggable: true
+    });
+
+    google.maps.event.addListener(marker, 'drag', function() {
+        setPosition(marker);
+
+    });
+
+    function setPosition(marker) {
+        var pos = marker.getPosition();
+        $('#lat').val(pos.lat());
+        $('#lng').val(pos.lng());
+    }
+
+    var geocoder = new google.maps.Geocoder();
+
+        $('#city').keypress(function(e) {
+            if (e.keyCode == 13) {
+                var request = {
+                    address : $(this).val()
+                }
+                geocoder.geocode(request, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var pos = results[0].geometry.location;
+                        map.setCenter(pos);
+                        marker.setPosition(pos);
+                        setPosition(marker);
+                    }
+                });
+                return false;
+            }
+        });
 });
