3588f104c76247f830b5cf435f3adc9da34e173a
MaxL34 <mlauvergeat@gmail.com>
2019-06-16 08:47:30 +0200

    Création du fichier et ajout du code

---
+++ js/eventAdder.js
@@ -0,0 +1,76 @@
+var eventToAdd = {
+    mapForm: $('#map-form'),
+    cityArea: $('#city'),
+    modalMap: $('#modal_map'),
+    modalText: $('#modal_text'),
+
+    init: function() {
+        var self = this;
+
+        $(document).click(function(event) { 
+            if(!$(event.target).closest(self.modal).length) {
+                self.modalMap.hide();
+                self.modalMap.stop(true, true).fadeOut();
+            } 
+        });
+
+        
+
+        self.mapForm.on('submit', function(e) {
+            e.stopPropagation();
+            e.preventDefault(); 
+
+            $.ajax({
+                url: '/nacomed/index.php?action=addEvent',
+                type: 'POST',
+                data: new FormData(this),
+                contentType: false,
+                cache: false,
+                processData:false,
+                dataType: 'text',
+                success: function(data) {
+                    console.log(data);
+                    
+                    switch(data) {
+                        case 'name_missing':
+                            self.modalText.text('Renseignez un nom pour l\'event');
+                            self.modalMap.show();
+                            self.modalMap.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalMap.hide();
+                            });
+                        break;
+
+                        case 'lat_missing':
+                            self.modalText.text('Renseignez une lattitude');
+                            self.modalMap.show();
+                            self.modalMap.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalMap.hide();
+                            });
+                        break;
+
+                        case 'lng_missing':
+                            self.modalText.text('Renseignez une longitude');
+                            self.modalMap.show();
+                            self.modalMap.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalMap.hide();
+                            });
+                        break;
+
+                        default:
+                            self.modalText.text('L\'évènement a bien été créé');
+                            self.modalMap.show();
+                            self.modalMap.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalMap.hide();
+                                window.location.href = "/nacomed/index.php?action=displayMapMgmt";
+                            });
+                    }
+
+                }
+            });
+        });
+    }
+};
