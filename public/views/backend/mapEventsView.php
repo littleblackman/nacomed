f3c6898c8b408dcb1bb5502682efec692b90bc68
MaxL34 <mlauvergeat@gmail.com>
2019-06-20 11:56:10 +0200

    Ajout de la modal_edit

--- public/views/backend/mapEventsView.php
+++ public/views/backend/mapEventsView.php
@@ -46,25 +46,26 @@
                 <div class="col-lg-12">
                     
                     <div class="events">
-                        <table>
+                        <table class="table">
                             <thead>
                                 <tr>
-                                    <th>Nom</th>
-                                    <th>Lattitude</th>
-                                    <th>Longitude</th>
-                                    <th>Actions</th>
+                                    <th scope="col">Nom</th>
+                                    <th scope="col">Embarquement</th>
+                                    <th scope="col">Lattitude</th>
+                                    <th scope="col">Longitude</th>
+                                    <th scope="col">Actions</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 <?php foreach ($mapEvents as $mE) {
-                                    echo '<tr class="row-map">';
-                                        echo '<td>' . $mE->event_name() . '</td>';
+                                    echo '<tr scope="row" class="row-map">';
+                                        echo '<td class="event_name">' . $mE->event_name() . '</td>';
+                                        echo '<td class="on_date">' . $mE->onboarding_date() . '</td>';
                                         echo '<td class="event_lat">' . $mE->event_lat() . '</td>';
                                         echo '<td class="event_lng">' . $mE->event_lng() . '</td>';
-                                        echo '<td class="buttons-map"><button type="submit" id="edit' . $mE->event_id() . '">Editer</button>';
-                                        echo '<button type="submit" id="delete' . $mE->event_id() . '">Supprimer</button>';
-                                        echo '<button type="submit" id="view' . $mE->event_id() . '">Voir</button></td>';
+                                        echo '<td class="buttons-map"><button id="edit' . $mE->event_id() . '">Editer</button>';
+                                        echo '<button id="delete' . $mE->event_id() . '">Supprimer</button>';
                                         echo '<input type="hidden" id="event_id" value="' . $mE->event_id() . '" />';
                                         echo '<tr></tr>';
                                     echo '</tr>';
@@ -83,6 +84,23 @@
             <div id="gmap" style="width:800px; height:600px; margin: 0 auto;">
             </div>
         </div>
+
+    <div id="modal_map" class="modal">
+        <div class="modal_content">
+            <p id="modal_text"></p>
+        </div>
+    </div>
+
+    <div id="modal_edit" class="modal">
+        <div class="modal_content_edit">
+            <span class="close">&times;</span>
+            <p id="modal_text_edit"></p>
+                <div id="edit_buttons">
+                    <button id="yes">Oui</button>
+                    <button id="no">Non</button>
+                </div>
+        </div>
+    </div>
 
     <?php require('./public/views/footer.php'); ?>
 
