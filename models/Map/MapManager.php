1f3604121db12f76efcc5ad2fafd5f31198110e1
MaxL34 <mlauvergeat@gmail.com>
2019-06-20 11:54:44 +0200

    Ajout de la fonction update et displayEditedEvent

--- models/Map/MapManager.php
+++ models/Map/MapManager.php
@@ -8,9 +8,9 @@ class MapManager {
         $this->setDb($db);
     }
 
-    public function addEvent($name, $lat, $lng) {
-        $q = $this->_db->prepare('INSERT INTO map_events (event_name, event_lat, event_lng) VALUES (?, ?, ?)');
-        $eventToAdd = $q->execute(array($name, $lat, $lng));
+    public function addEvent($name, $lat, $lng, $date) {
+        $q = $this->_db->prepare('INSERT INTO map_events (event_name, event_lat, event_lng, onboarding_date) VALUES (?, ?, ?, ?)');
+        $eventToAdd = $q->execute(array($name, $lat, $lng, $date));
     }
 
     public function displayEvents() {
@@ -23,6 +23,26 @@ class MapManager {
         }
         return $events;
     }
+
+    public function displayEditedEvent($id) {
+        $q = $this->_db->prepare('SELECT * FROM map_events WHERE event_id = ?');
+        $q->execute(array($id));
+
+        $data = $q->fetch();
+                
+        return $data;
+    }
+
+    public function updateEvent($name, $lat, $lng, $date, $id) {
+        $q = $this->_db->prepare('UPDATE map_events SET event_name = ?, event_lat = ?, event_lng = ?, onboarding_date = ?  WHERE event_id = ?');
+        $q->execute(array($name, $lat, $lng, $date, $id));
+    }
+
+    public function deleteEvent($event_id) {
+        $q = $this->_db->prepare('DELETE FROM map_events WHERE event_id = ?');
+        $eventToDelete = $q->execute(array($event_id));
+    }
+
 
     public function setDb(PDO $db) {
         $this->_db = $db;
