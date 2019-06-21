e90ce6e5a80f652bbb6e79a7aabf0dc0646c6423
MaxL34 <mlauvergeat@gmail.com>
2019-06-21 15:02:16 +0200

    Modification de la structure

--- public/views/frontend/newsView.php
+++ public/views/frontend/newsView.php
@@ -28,24 +28,29 @@
 
         <div class="container news-view">
             <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-heading">
-                    <h1><?php echo $article->art_title(); ?></h1>
-                    <span class="meta">Publié le : <?php echo $article->date_fr(); ?></span>
+                <div class="col-lg-12 col-md-10 mx-auto news-display">
+                    <div class="news-img">
+                        <img src="<?php echo $article->url_img(); ?>" alt="<?php echo $article->art_title(); ?>" />
                     </div>
+                    <div class="news-heading">
+                        <h1><?php echo $article->art_title(); ?></h1>
+                        <span class="meta">Publié le : <?php echo $article->date_fr(); ?></span>
+                    </div>
+
+                    <!-- Post Content -->
+                    <article class="art-content">
+                        <div class="container news-content">
+                            <div class="row">
+                                <div class="col-lg-8 col-md-10 mx-auto">
+                                    <p><?php echo $article->art_content(); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
                 </div>
             </div>
 
-            <!-- Post Content -->
-            <article>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-10 mx-auto">
-                            <p><?php echo $article->art_content(); ?></p>
-                        </div>
-                    </div>
-                </div>
-            </article>
+            
 
             <div id="add_com">
                 <button>Ajouter un commentaire</button>
