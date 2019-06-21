89ee01872b6278fc3e8a37c3a05907c66ab90cdb
MaxL34 <mlauvergeat@gmail.com>
2019-06-21 15:07:46 +0200

    AJout du code pour ajouter des infos au programme du navire

---
+++ js/programInfosAdder.js
@@ -0,0 +1,62 @@
+var programInfos = {
+    programForm: $('#program-form'),
+    modalProg: $('#modal_prog'),
+    modalText: $('#modal_text'),
+
+    init: function() {
+        var self = this;
+
+        $(document).click(function(event) { 
+            if(!$(event.target).closest(self.modal).length) {
+                self.modalProg.hide();
+                self.modalProg.stop(true, true).fadeOut();
+            } 
+        });
+
+        self.programForm.on('submit', function(e) {
+            e.preventDefault(); 
+
+            $.ajax({
+                url: '/nacomed/index.php?action=addProgInfos',
+                type: 'POST',
+                data: new FormData(this),
+                contentType: false,
+                cache: false,
+                processData:false,
+                dataType: 'text',
+                success: function(data) {
+                    console.log(data);
+                    switch (data) {
+                        case 'badMayWeek':
+                            self.modalText.text('Erreur dans le numéro de semaine pour le mois de Mai');
+                            self.modalProg.show();
+                            self.modalProg.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalProg.hide();
+                            });
+                        break;
+
+                        case 'missing_infos':
+                            self.modalText.text('Attention, vous n\'avez rentré aucune information');
+                            self.modalProg.show();
+                            self.modalProg.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalProg.hide();
+                            });
+                        break;
+
+                        default: 
+                            self.modalText.text('Les informations ont bien été ajoutées');
+                            self.modalProg.show();
+                            self.modalProg.fadeOut(4000, function() {
+                                self.modalText.text('');
+                                self.modalProg.hide();
+                            });
+                    }
+                }
+            });
+                    
+        });
+
+    }
+};
