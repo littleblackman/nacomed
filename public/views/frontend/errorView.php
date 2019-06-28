b6c998f2801c2abd7e4b624ac67642715b225aa8
MaxL34 <mlauvergeat@gmail.com>
2019-06-28 08:48:51 +0200

    Création du fichier et écriture du code

---
+++ public/views/frontend/errorView.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Association Nacomed : Accueil</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+        <!-- Google fonts-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo:400,400i">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+        <!-- OwlCarousel -->
+        <link rel="stylesheet" href="./vendor/owl.carousel/assets/owl.carousel.css">
+        <link rel="stylesheet" href="./vendor/owl.carousel/assets/owl.theme.default.min.css">
+        <!-- Font Awesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Parallax-->
+        <link rel="stylesheet" href="./vendor/onepage-scroll/onepage-scroll.css">
+        <!-- theme stylesheet-->
+        <link rel="stylesheet" href="./css/style.blue.css" id="theme-stylesheet">
+        <!-- Custom stylesheet - for your changes-->
+        <link rel="stylesheet" href="./css/custom.css">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="favicon.png">
+        <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    </head>
+    <body>
+        <div class="error-message">
+            <p><?php echo $errorMessage; ?></p>
+        </div>
+    </body>
+</html>
