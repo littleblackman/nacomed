7d21b5944e1f7dc98060992cff838d094c49f25f
MaxL34 <mlauvergeat@gmail.com>
2019-07-01 10:26:46 +0200

    AJout de code permettant de cacher la div de vidéo si aucune vidéo n'a été ajoutée et de l'afficher dans le cas contraire

--- js/controllers/newsView.js
+++ js/controllers/newsView.js
@@ -4,4 +4,10 @@ $(function() {
 
     var reportedCom = Object.create(comToReport);
     reportedCom.init();
+
+    if ($('#video-iframe').attr('src') === '') {
+        $('.video').hide();
+    } else {
+        $('.video').show();
+    }
 });
