82d9786061f169a28aca710837ceeeba32722196
MaxL34 <mlauvergeat@gmail.com>
2019-07-09 16:45:08 +0200

    Modificatino de la page pour le site en ligne

--- public/views/frontend/supportUsView.php
+++ public/views/frontend/supportUsView.php
@@ -42,7 +42,11 @@
         <!-- Navigation -->
         <?php require('./public/views/header.php'); ?>
 
-        <iframe id="haWidget" allowtransparency="true" scrolling="auto" src="https://www.helloasso.com/associations/nacomed/formulaires/1/widget" style="width:100%;height:750px;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe>
+        <div class="donation">
+            <p>A VENIR : VOUS POURREZ SOUTENIR L'ASSOCIATION EN FAISANT DES DONS</p>
+        </div>
+
+        <!-- <iframe id="haWidget" allowtransparency="true" scrolling="auto" src="https://www.helloasso.com/associations/nacomed/formulaires/1/widget" style="width:100%;height:750px;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe> -->
 
 
         <?php require('./public/views/footer.php'); ?>
