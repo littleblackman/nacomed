5ccae4aa83244345460eab1c34e84bba625866a8
MaxL34 <mlauvergeat@gmail.com>
2019-07-15 15:23:07 +0200

    Création d'un modèle pour la connexion à la bdd

---
+++ models/Managers/Manager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Manager;
+
+class Manager {
+    protected function dbConnect() {
+        try {
+            include('./config.php');
+            $db = new \PDO('mysql:host=' . $localhost . ';dbname=' . $dbName . '; charset=utf8' , '' . $loginLocal . '', ''. $pwdLocal . '');
+            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+            return $db;
+        } catch(Exception $e) {
+            $errorMessage = $e->getMessage();
+            require('./public/views/frontend/errorView.php');
+        }
+    }
+}
